179550da66f26caace22c1f5a4b68bda5f48d290
Yiyuan Yang <yiyuan.yang@vanderbilt.edu>
2020-04-18 11:15:54 -0700

    Finished logger

---
+++ train/exp001/config_reader.py
@@ -0,0 +1,16 @@
+"""
+    Content: Reading Configs for exp001
+    Author: Yiyuan Yang
+    Date: April. 18th 2020
+"""
+
+import yaml
+
+class ConfigReader(object):
+    def __init__(
+        self,
+        config_path,
+        logger = None
+    ):
+        
+
