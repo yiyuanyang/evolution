f6e3b72a4659ded82aee373f436fe29421d85209
Yiyuan Yang <yiyuan.yang@vanderbilt.edu>
2020-05-13 22:51:53 -0700

    Synthesis net stuff

---
+++ model/models/synthesis_net/synthesis_net.py
@@ -0,0 +1,30 @@
+"""
+    Content: A take on synthesis net
+    Author: Yiyuan Yang
+    Date: May 13th 2020
+"""
+
+import torch
+import torch.nn as nn
+
+
+class SynthesisNet(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        image_size,
+        num_classes
+    ):
+        return 0
+        # ! Under Construction
+
+    def make_block(
+        self,
+        in_channels,
+        out_channels,
+    ):
+        # ! Under Construction
+
+
+
+
